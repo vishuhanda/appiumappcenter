7ff10e99d4b57c6509c39226a350152332082ce3
vishu <itsvishuhanda@gmail.com>
2021-07-07 20:50:46 +0530

    trying using testng tests

--- src/test/java/Tests/DemoTest.java
+++ src/test/java/Tests/DemoTest.java
@@ -2,9 +2,9 @@ package Tests;
 
 import com.microsoft.appcenter.appium.Factory;
 import org.junit.Rule;
-import org.junit.Test;
+//import org.junit.Test;
 import org.junit.rules.TestWatcher;
-//import org.testng.annotations.Test;
+import org.testng.annotations.Test;
 import pages.DemoPageTest;
 import utils.DriverManager;
 import utils.PropertyManager;
@@ -14,8 +14,8 @@ import java.net.MalformedURLException;
 public class DemoTest {
 
 
-    @Rule
-    public TestWatcher watcher = Factory.createWatcher();
+//    @Rule
+//    public TestWatcher watcher = Factory.createWatcher();
 
     @Test
     public void firstTest() throws MalformedURLException, InterruptedException {
@@ -30,10 +30,10 @@ public class DemoTest {
         System.out.println("First test");
 
         Thread.sleep(5000);
-        DriverManager.setLabel("Ending app test 1");
-        DriverManager.setLabel("Ending app test 2");
-        DriverManager.setLabel("Ending app test 3");
-        DriverManager.setLabel("Ending app test 4");
+//        DriverManager.setLabel("Ending app test 1");
+//        DriverManager.setLabel("Ending app test 2");
+//        DriverManager.setLabel("Ending app test 3");
+//        DriverManager.setLabel("Ending app test 4");
         DriverManager.tearDownDriver();
 
     }
